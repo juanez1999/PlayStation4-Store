b542ad49688c2c7af8eb3d41a066f9026a6e1711
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-10-29 19:21:57 -0500

    Se agrega a la base de datos los productos que selecciona al agregar al carrito

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
-const ObjectID = require ('mongodb').ObjectID;
+const ObjectID = require('mongodb').ObjectID;
 
-function createRoutes (app, db) {
+function createRoutes(app, db) {
 
     app.get('/', (request, response) => {
         console.log('alguien entró a la ruta inicial');
@@ -13,119 +13,131 @@ function createRoutes (app, db) {
         response.render('store');
     });
 
-    app.get('/api/carItems', (request, response) => {
+    app.get('/api/carItemss', (request, response) => {
         const products = db.collection('carItems');
-        
+
         //buscamos todos los productos
         products.find({})
-        //transformamos el cursor a una arreglo
-        .toArray((err,result) => {
-            //aseguramos de que no hay error
-            assert.equal(null,err);
-            
-            response.send(result);
-        });
+            //transformamos el cursor a una arreglo
+            .toArray((err, result) => {
+                //aseguramos de que no hay error
+                assert.equal(null, err);
+
+                response.send(result);
+            });
     });
 
     app.post('/api/carItems', (request, response) => {
         const products = db.collection('carItems');
-        
+        //buscamos todos los productos
+        products.find({}).toArray((err, result) => {
+            //aseguramos de que no hay error
+            assert.equal(null, err);
+            result[0].products.push(request.body.idProduct);
+
+            products.updateOne({ id: 0 },
+                {
+                    $set: { products: result[0].products }
+                }
+            );
+
+            response.send({
+                message: 'ok',
+            });
+        });
 
         //push para meter el arreglo de id y update para actualizarlo
-        products.push(request.body);
-        
-         response.send({
-            message: 'ok'
-         });
+        //products.push(request.body);
+
     });
 
     app.get('/producto/:id', (request, response) => {
         console.log('alguien entró al producto');
 
         const products = db.collection('products');
-        
-        //buscamos todos los productos
-        products.find({_id: new ObjectID(request.params.id)}).toArray((err,result) =>{
 
-            assert.equal(null,err);
+        //buscamos todos los productos
+        products.find({ _id: new ObjectID(request.params.id) }).toArray((err, result) => {
+
+            assert.equal(null, err);
 
             var context = {
                 product: result[0],
             };
 
             console.log(result);
-            response.render('product',context);
+            response.render('product', context);
         });
 
-        
+
     });
 
     app.get('/api/productsItems', (request, response) => {
         const products = db.collection('products');
-        
+
         //buscamos todos los productos
         products.find({})
-        //transformamos el cursor a una arreglo
-        .toArray((err,result) => {
-            //aseguramos de que no hay error
-            assert.equal(null,err);
-            
-            response.send(result);
-        });
+            //transformamos el cursor a una arreglo
+            .toArray((err, result) => {
+                //aseguramos de que no hay error
+                assert.equal(null, err);
+
+                response.send(result);
+            });
     });
 
     app.get('/api/products', (request, response) => {
         const products = db.collection('products');
 
-        if(request.query.orderType == 'priceAscending'){
-            products.find().sort({price:1})
-            .toArray((err,result)=>{
-                assert.equal(null,err);
-            
-                response.send(result);
-            });
+        if (request.query.orderType == 'priceAscending') {
+            products.find().sort({ price: 1 })
+                .toArray((err, result) => {
+                    assert.equal(null, err);
+
+                    response.send(result);
+                });
             return;
         }
 
-        if(request.query.orderType == 'priceFalling'){
-            products.find().sort({price:-1})
-            .toArray((err,result)=>{
-                assert.equal(null,err);
-            
-                response.send(result);
-            });
+        if (request.query.orderType == 'priceFalling') {
+            products.find().sort({ price: -1 })
+                .toArray((err, result) => {
+                    assert.equal(null, err);
+
+                    response.send(result);
+                });
             return;
 
         }
 
-        if(request.query.orderType == 'Offers'){
-            products.find().sort({price:-1})
-            .toArray((err,result)=>{
-                assert.equal(null,err);
-            
-                response.send(result);
-            });
+        if (request.query.orderType == 'Offers') {
+            products.find().sort({ price: -1 })
+                .toArray((err, result) => {
+                    assert.equal(null, err);
+
+                    response.send(result);
+                });
             return;
         }
 
-        if(request.query.orderType == 'Popularity'){
-            products.find().sort({popularity:-1})
-            .toArray((err,result)=>{
-                assert.equal(null,err);
-            
-                response.send(result);
-            });
+        if (request.query.orderType == 'Popularity') {
+            products.find().sort({ popularity: -1 })
+                .toArray((err, result) => {
+                    assert.equal(null, err);
+
+                    response.send(result);
+                });
             return;
         }
 
         products.find({})
-        //transformamos el cursor a una arreglo
-        .toArray((err,result) => {
-            //aseguramos de que no hay error
-            assert.equal(null,err);
-            
-            response.send(result);
-        });
+            //transformamos el cursor a una arreglo
+            .toArray((err, result) => {
+                //aseguramos de que no hay error
+                assert.equal(null, err);
+
+                response.send(result);
+            });
     });
 
 
