13c62627887ddb32307d1dbc0d3336b71f21d510
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-11-28 13:29:11 -0500

    Se agrega la versión responsive del producto a detalle

--- public/scripts/productDetails.js
+++ public/scripts/productDetails.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function(){
     
     var addToCart = document.querySelector(".productDetail__featuresAddToCart");
+    var addToCart2 = this.document.querySelector(".productDetail__AddToCart");
     
     var tl = gsap.timeline({});
     tl.to(".navGlobal__shop",{scale: .6 , duration: 0.3,});
@@ -35,12 +36,39 @@ window.addEventListener('load', function(){
             displayCar();
         });
     });
+
+    addToCart2.addEventListener("click",function() {
+        console.log("sirve el boton");
+        console.log(addToCart.getAttribute("data-id"));
+        
+        var data= new URLSearchParams();
+        data.append("idProduct",addToCart.getAttribute("data-id"));
+        
+        var promise= fetch('/api/carItems', {
+            method : 'POST', 
+            body : data
+        });
+        
+        promise.then((raw) => {
+            return raw.json();
+        }).then((info) => {
+            displayCar();
+        });
+    });
     
     addToCart.addEventListener("mousedown",function() {
         playAnimation();
     });
     
     addToCart.addEventListener("mouseup",function() {
+        rewindAnimation();
+    });
+
+    addToCart2.addEventListener("mousedown",function() {
+        playAnimation();
+    });
+    
+    addToCart2.addEventListener("mouseup",function() {
         rewindAnimation();
     });
     
